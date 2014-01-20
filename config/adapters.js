c9d46a6305cf3c9b3dfc8bc79a2444760bc8d876
Barth <barthelemy.jamois@mines-ales.org>
2014-01-20 11:36:18 +0100

    adapters for heroku

--- config/adapters.js
+++ config/adapters.js
@@ -45,7 +45,7 @@ module.exports.adapters = {
   mongo: {
     module: 'sails-mongo',
     schema: false,
-    url: process.env.MONGOLAB_URI || 'mongodb://localhost:27017/creaktive' 
+    url: process.env.MONGOLAB_URI
   },
   //
 
