770092449fe9f8851017b41af081a286a38779a5
Barth <barthelemy.jamois@mines-ales.org>
2014-01-29 18:47:52 +0100

    test

--- config/session.js
+++ config/session.js
@@ -35,10 +35,9 @@ module.exports.session = {
 
 
   // Uncomment the following lines to use your Mongo adapter as a session store
-  // adapter: 'mongo',
-  // url : 'mongodb://localhost:27017/creaktive' || process.env.MONGOLAB_URI,
-  // db: 'creaktive',
-  // collection: 'sessions',
+  adapter: 'mongo',
+  url : process.env.MONGOHQ_URL || 'mongodb://localhost:27017/creaktive' ,
+  collection: 'sessions',
   //
   // Optional Values:
   //
