056979a5fc7278a42bb1e146568b85859bf2762d
Barth <barthelemy.jamois@mines-ales.org>
2014-02-04 18:01:27 +0100

    force https but not on local

--- api/policies/forceHTTPs.js
+++ api/policies/forceHTTPs.js
@@ -1,12 +1,16 @@
 module.exports = function(req,res,next){
 	var url = req.headers.referer;
-	if(url != "http://localhost:1337/"){
+	console.log(url)
+	if(url ==="http://localhost:1337/") {
+		next();
+	}
+	else{
 		if(req.headers['x-forwarded-proto']!='https'){
 			res.redirect(url.substring(0,url.length-1)+req.url)
 		}
-		else
+		else{
 			next()
+		}
 	}
-	else
-		next() /* Continue to other routes if we're not redirecting */
+
 };
