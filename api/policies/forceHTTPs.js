49d8bd1dbf080375838c13d838b7c86ef8879357
Barth <barthelemy.jamois@mines-ales.org>
2014-02-04 17:28:32 +0100

    test websocket ssl

--- api/policies/forceHTTPs.js
+++ api/policies/forceHTTPs.js
@@ -1,6 +1,6 @@
 module.exports = function(req,res,next){
     if(req.headers['x-forwarded-proto']!='https')
-      res.redirect('https://creaktive.herokuapp.com/'+req.url)
+      res.redirect('https://creaktive.herokuapp.com'+req.url)
     else
       next() /* Continue to other routes if we're not redirecting */
   };
