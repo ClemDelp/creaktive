ea81c35819846213a6e7b9fac0265a893045cee4
Barth <barthelemy.jamois@mines-ales.org>
2014-02-04 17:54:42 +0100

    force https but not on local

--- api/policies/forceHTTPs.js
+++ api/policies/forceHTTPs.js
@@ -1,10 +1,12 @@
 module.exports = function(req,res,next){
-    var url = req.headers.referer;
-    if(!url === "http://localhost:1337/"){
-    	    if(req.headers['x-forwarded-proto']!='https'){
-
-      res.redirect(url.substring(0,url.length-1)+req.url)
-    }
-    }
-      next() /* Continue to other routes if we're not redirecting */
-  };
+	var url = req.headers.referer;
+	if(!url === "http://localhost:1337/"){
+		if(req.headers['x-forwarded-proto']!='https'){
+			res.redirect(url.substring(0,url.length-1)+req.url)
+		}
+		else
+			next()
+	}
+	else
+		next() /* Continue to other routes if we're not redirecting */
+};
