a24f273668f10d4cac0233b5a81adae53c29c00c
Barth <barthelemy.jamois@mines-ales.org>
2014-02-11 09:00:46 +0100

    replace http baseurl forecehttps

--- api/policies/forceHTTPs.js
+++ api/policies/forceHTTPs.js
@@ -1,15 +1,14 @@
 module.exports = function(req,res,next){
 	console.log(req.baseUrl, req.url )
-			    	var u = req.baseUrl.replace("http","https");
-		    	u.slice(0, u.lastIndexOf(":"))
-		    	console.log(u)
+		var u = req.baseUrl.replace("http","https");
+
 	if(req.baseUrl == "http://localhost:1337"){
 		next()
 	} 
 	else{
 	    if(req.headers['x-forwarded-proto']!='https'){
 	    	var u = req.baseUrl.replace("http","https");
-	    	u.slice(0, u.lastIndexOf(":"))
+	    	u = u.slice(0, u.lastIndexOf(":"))
 			res.redirect(u+req.url)
 	    }
 	    else{
