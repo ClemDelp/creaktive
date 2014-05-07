3e9c0f436e9c6cd1901baf766c6479ce9c46a8fc
barth30 <barthelemy.jamois@mines-ales.org>
2014-05-07 11:11:08 +0200

    Ajout d'un serveur mail externe

--- api/policies/canWrite.js
+++ api/policies/canWrite.js
@@ -4,7 +4,6 @@ module.exports = function(req, res, next) {
 	'use strict';
 
 		var project_id = "";
-		console.log(req.session.currentProject);
 	if(req.session.currentProject) project_id = req.session.currentProject.id;
 	else if (req.query.projectId) project_id = req.query.projectId
 	else if(req.body.params.projectId) project_id = req.body.params.projectId;
@@ -15,7 +14,6 @@ module.exports = function(req, res, next) {
 	}).done( function (err, perm){
 		if(err) next(err);
 		if(perm.length !== 0){
-			console.log(perm)
 			if (perm[0].right == "rw" || perm[0].right == "admin") next();
 			else res.send({err : "You have read-only permission" });
 		} 
