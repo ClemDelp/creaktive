46d8a4acb19722dbbdb42f614db94c9c78f9181c
barth30 <barthelemy.jamois@mines-ales.org>
2014-05-12 09:45:49 +0200

    rien

--- api/policies/canWrite.js
+++ api/policies/canWrite.js
@@ -6,7 +6,6 @@ module.exports = function(req, res, next) {
 	var project_id = "";
 	if(req.body.params.project && req.body.params !="") project_id = req.body.params.project;
 	else project_id = req.session.currentProject.id;
-	console.log(req.body)
 	
 	Permission.find({
 		user_id : req.session.user.id,
