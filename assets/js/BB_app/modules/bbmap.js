ed5a51350fb3c0d905e07f0ccbfe1acc13b1faed
ClemNimes <delpuech.clement@gmail.com>
2014-05-12 12:09:17 +0200

    bbmap update

--- assets/js/BB_app/modules/bbmap.js
+++ assets/js/BB_app/modules/bbmap.js
@@ -171,6 +171,23 @@ bbmap.Views.Main = Backbone.View.extend({
     events : {
         "click .addUnlinked" : "addUnlinkedConcept",
     },
+    setZoom : function(zoom) {
+      transformOrigin = [ 0.5, 0.5 ];
+      el = this.el;
+      var p = [ "webkit", "moz", "ms", "o" ],
+          s = "scale(" + zoom + ")",
+          oString = (transformOrigin[0] * 100) + "% " + (transformOrigin[1] * 100) + "%";
+
+      for (var i = 0; i < p.length; i++) {
+        el.style[p[i] + "Transform"] = s;
+        el.style[p[i] + "TransformOrigin"] = oString;
+      }
+
+      el.style["transform"] = s;
+      el.style["transformOrigin"] = oString;
+
+      this.instance.setZoom(zoom);    
+    },
     addUnlinkedConcept : function(e){
         e.preventDefault();
 
@@ -309,3 +326,22 @@ bbmap.Views.Main = Backbone.View.extend({
         return this;
     }
 });
+
+
+window.setZoom = function(zoom, instance, transformOrigin, el) {
+  transformOrigin = transformOrigin || [ 0.5, 0.5 ];
+  el = el || instance.Defaults.Container;
+  var p = [ "webkit", "moz", "ms", "o" ],
+      s = "scale(" + zoom + ")",
+      oString = (transformOrigin[0] * 100) + "% " + (transformOrigin[1] * 100) + "%";
+
+  for (var i = 0; i < p.length; i++) {
+    el.style[p[i] + "Transform"] = s;
+    el.style[p[i] + "TransformOrigin"] = oString;
+  }
+
+  el.style["transform"] = s;
+  el.style["transformOrigin"] = oString;
+
+  instance.setZoom(zoom);    
+};
