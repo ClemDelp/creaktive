a2dcf92aee271dace66b0bf0afcea66eaa74e746
Barth-portable <barthelemy.jamois@creaktive.fr>
2014-10-13 08:45:33 +0200

    Screenshot - modification du delay de pageres pour arriver à prendre la page une fois qu'elle est chargée

--- api/controllers/BBmapController.js
+++ api/controllers/BBmapController.js
@@ -58,7 +58,7 @@ module.exports = {
     
 
     var pageres = new Pageres({
-        delay: 30, 
+        delay: 10, 
         cookies : [cookie],
         selector : ".bulle",
         filename : req.session.currentProject.id + ".png"
