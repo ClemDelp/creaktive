6a30d18f22ed387ff2bbab7669cc9acb5dee2550
barth nimes <barthelemy.jamois@creaktive.fr>
2014-10-16 18:03:43 +0200

    Screenshot test sans https

--- api/policies/forceHTTPs.js
+++ api/policies/forceHTTPs.js
@@ -1,21 +1,19 @@
 module.exports = function(req,res,next){
 	//return next()
-	// 	var u = req.baseUrl.replace("http","https");
+		var u = req.baseUrl.replace("http","https");
 
-	// if(req.baseUrl == "http://localhost:1337" ||req.baseUrl == "http://localhost:5000"  ){
-	// 	next()
-	// } 
-	// else{
-	//     if(req.headers['x-forwarded-proto']!='https'){
-	//     	var u = req.baseUrl.replace("http","https");
-	//     	u = u.slice(0, u.lastIndexOf(":"))
-	// 		res.redirect(u+req.url)
-	//     }
-	//     else{
-	//       next() /* Continue to other routes if we're not redirecting */
-	// 	}
-	// }
-
-	next();
+	if(req.baseUrl == "http://localhost:1337" ||req.baseUrl == "http://localhost:5000"  ){
+		next()
+	} 
+	else{
+	    if(req.headers['x-forwarded-proto']!='https'){
+	    	var u = req.baseUrl.replace("http","https");
+	    	u = u.slice(0, u.lastIndexOf(":"))
+			res.redirect(u+req.url)
+	    }
+	    else{
+	      next() /* Continue to other routes if we're not redirecting */
+		}
+	}
 
   };
