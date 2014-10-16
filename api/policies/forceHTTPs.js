98f185a50f98bf7c238e5ecf533e5a240244cb7e
barth nimes <barthelemy.jamois@creaktive.fr>
2014-10-16 18:10:25 +0200

    Screenshot test modification de la policy force https

--- api/policies/forceHTTPs.js
+++ api/policies/forceHTTPs.js
@@ -1,6 +1,5 @@
 module.exports = function(req,res,next){
 	//return next()
-		var u = req.baseUrl.replace("http","https");
 
 	if(req.baseUrl == "http://localhost:1337" ||req.baseUrl == "http://localhost:5000"  ){
 		next()
@@ -9,9 +8,12 @@ module.exports = function(req,res,next){
 	    if(req.headers['x-forwarded-proto']!='https'){
 	    	var u = req.baseUrl.replace("http","https");
 	    	u = u.slice(0, u.lastIndexOf(":"))
-			res.redirect(u+req.url)
+			var url = u + req.url;
+			console.log("Redirect to ", url )
+			res.redirect(url)
 	    }
 	    else{
+	    	console.log("Proceding")
 	      next() /* Continue to other routes if we're not redirecting */
 		}
 	}
