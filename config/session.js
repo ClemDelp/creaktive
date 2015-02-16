58091f4cd9519f27124a17ed9111b5e1f0238827
Clément Delpuech <yodelpuech.clement@gmail.com>
2015-02-16 13:56:31 +0100

    trackjs + session

--- config/session.js
+++ config/session.js
@@ -16,7 +16,7 @@ module.exports.session = {
   // Replace at your own risk in production-- you will invalidate the cookies of your users,
   // forcing them to log in again. 
   secret: 'c17007f333b6f77636f34e979f2ccbcf',
-  version : 51,
+  version : 52,
 
 
   // In production, uncomment the following lines to set up a shared redis session store
