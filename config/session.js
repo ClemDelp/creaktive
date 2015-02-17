c0f5b25755d376f9b7bf5c7a9900256c17fd3e4c
Clément Delpuech <yodelpuech.clement@gmail.com>
2015-02-17 18:21:13 +0100

    Merge branch 'bbmap_draft' into heroku
    Conflicts:
    	config/session.js

--- config/session.js
+++ config/session.js
@@ -16,12 +16,8 @@ module.exports.session = {
   // Replace at your own risk in production-- you will invalidate the cookies of your users,
   // forcing them to log in again. 
   secret: 'c17007f333b6f77636f34e979f2ccbcf',
-<<<<<<< HEAD
-  version : 52,
+  version : 57,
 
-=======
-  version : 54,
->>>>>>> bbmap_draft
 
   // In production, uncomment the following lines to set up a shared redis session store
   // that can be shared across multiple Sails.js servers
