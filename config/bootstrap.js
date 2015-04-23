b5c7267f363f30dcbfb25d669b9f2f2efe7b0a02
barth@mac <barthelemy.jamois@creaktive.fr>
2015-04-23 18:07:06 +0200

    modification page login 2

--- config/bootstrap.js
+++ config/bootstrap.js
@@ -8,24 +8,14 @@
  * http://sailsjs.org/#documentation
  */
 
- var memwatch = require('memwatch');
+
 
 module.exports.bootstrap = function (cb) {
 
 	sails.config.appName =  "CreaKtive";
 
 
-	memwatch.on('leak', function(info) { 
-		console.log("MEMORY LEAK");
-		console.log(info)
 
-	 });
-
-	memwatch.on('stats', function(stats) { 
-		console.log("MEMORY STATS");
-		console.log(stats)
-
-	 });
 
 
   // It's very important to trigger this callack method when you are finished 
